2f85aa799b854d274c2a40e7da2a9c1788779be2
Achilles06 <92751219+Achilles06@users.noreply.github.com>
2024-06-23 20:47:55 -0400

    commit

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import mood_responses
-
+import text_utils as tu
 if __name__ == "__main__":
     mood = input("How are you feeling today? ")
     print(mood_responses.mood_response(mood))
+
+    sample_string = input("Enter a string: ")
+
+    reversed_string = tu.reverse_string(sample_string)
+    capitalized_string = tu.capitalize_string(sample_string)
+
+    print(f"Reversed String: {reversed_string}")
+    print(f"Capitalized String: {capitalized_string}")
